3763e53d0a7006b9fdfe70b7f8252dd2ca69be32
Ivaylo Dankolov <ivodankolov@gmail.com>
2025-11-14 00:58:26 +0200

    27 solution. Ah, C++, my brute force darling

---
+++ cpp/problem27.cpp
@@ -0,0 +1,60 @@
+#include <vector>
+#include <iostream>
+
+void populate_sieve(std::vector<bool>& is_prime, int total) {
+    is_prime.resize(total + 1, true);
+    is_prime[0] = false;
+    is_prime[1] = false;
+
+    for (int i = 2; 2 * i <= total; ++i) {
+        if (is_prime[i]) {
+            for (int j = 2 * i; j <= total; j += i) {
+                is_prime[j] = false;
+            }
+        }
+    }
+}
+
+int quadratic_consec_primes(int a, int b, const std::vector<bool>& is_prime) {
+    /*
+    (n+1) ^ 2 + a * (n+1) + b = n^2 + 2n + 1 + a*n + a + b
+        = n^2 + a*n + b + 2n + a + 1
+        = P(n) + 2n + a + 1
+
+    Thus we can compute it recursively. Does it make any sense to do so? Not really, but it's a fun fact.
+    */
+    int n = 0;
+    int fn = b;
+    while(is_prime[fn]) {      
+        fn += 2 * n + a + 1;
+        ++n;
+    }
+    return n;
+}
+
+int main() {
+
+    int total_primes = 6 * 1000000; // I don't think we'll need quite that many but C++ goes brrr
+
+    std::vector<bool> is_prime;
+
+    populate_sieve(is_prime, total_primes);
+
+    int max_consec = 0;
+    int max_a = 0;
+    int max_b = 0;
+    for(int a = -999; a < 1000; ++a) {
+        for(int b = -1000; b <= 1000; ++b) {
+            int consec = quadratic_consec_primes(a, b, is_prime);
+            if(consec > max_consec) {
+                max_consec = consec;
+                max_a = a;
+                max_b = b;
+            }
+        }
+    }
+
+    std::cout << "Max consecutive primes: " << max_consec << " for a = " << max_a << ", b = " << max_b << std::endl;
+    std::cout << max_a * max_b << std::endl;
+
+}
